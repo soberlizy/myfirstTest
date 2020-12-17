7e8c136eb17977e1b97a4a913dc63afb5b4ded7e
soberlizy <2470334493@qq.com>
2020-12-17 14:52:47 +0800

    audio

--- js/WYY.js
+++ js/WYY.js
@@ -8,7 +8,7 @@
      ["http://p1.music.126.net/7AQR8rnStD5Hgu6_n12sqA==/109951165540302472.jpg?imageView&quality=89"],
      ["http://p1.music.126.net/AaeF99qPWALAr4y_Y11TfQ==/109951165539819938.jpg?imageView&quality=89"],
      ["http://p1.music.126.net/dAL7PsAhV85t0Uwk63hrDw==/109951165539765762.jpg?imageView&quality=89"],
-     ["http://p1.music.126.net/kTrKiEURddarIEoDKYOwWA==/109951165528290386.jpg?imageView&quality=89"],
+     ["http://p1.music.126.net/kfpjIf7uI2U3DdpwmheCgQ==/109951165546835368.jpg?imageView&quality=89"],
      ["http://p1.music.126.net/8tI6FrHD51T1kw90BapSwQ==/109951165540314347.jpg?imageView&quality=89"],
  ]
  var imgsbg=[
@@ -20,7 +20,7 @@
      ["http://p1.music.126.net/7AQR8rnStD5Hgu6_n12sqA==/109951165540302472.jpg?imageView&blur=40x20"],
      ["http://p1.music.126.net/AaeF99qPWALAr4y_Y11TfQ==/109951165539819938.jpg?imageView&blur=40x20"],
      ["http://p1.music.126.net/dAL7PsAhV85t0Uwk63hrDw==/109951165539765762.jpg?imageView&blur=40x20"],
-     ["http://p1.music.126.net/kTrKiEURddarIEoDKYOwWA==/109951165528290386.jpg?imageView&blur=40x20"],
+     ["http://p1.music.126.net/kfpjIf7uI2U3DdpwmheCgQ==/109951165546835368.jpg?imageView&blur=40x20"],
      ["http://p1.music.126.net/8tI6FrHD51T1kw90BapSwQ==/109951165540314347.jpg?imageView&blur=40x20"],
  ]
  //获取标签
@@ -106,5 +106,3 @@ nextbtn.onclick=function next(){
     changeicon(m)
 }
 /*轮播图的js完结 */
-
-
--- js/music.js
+++ js/music.js
@@ -3,14 +3,75 @@ function $(selector) {
     return selector.substring(0, 1) == '.' ?
         document.getElementsByClassName(selector.substring(1)) :
         document.getElementById(selector.substring(1));
-    // return document.querySelector(selector);
 }
+// 歌曲的信息
 var musics = [
     {
         name: "Smile Like You Used To",
         author: "Tim Walker",
         path: "/music/Tim Walker - Smile Like You Used To.mp3",
         img: "http://p3.music.126.net/m9CkDd3lfcR4IFSbBGKq4w==/2542070883708394.jpg?param=34y34"
+        
+    },
+    {
+        name: "海阔天空",
+        author: "beyond",
+        path: "/music/海阔天空 (网络电影《九五2班》插曲)-Beyond.mp3",
+        img: "https://p2.music.126.net/ghmbmEQS-IJfZPjdA3KGxg==/82463372084291.jpg?param=34y34"
+    },
+    {
+        name: "光辉岁月",
+        author: "beyond",
+        path: "/music/光辉岁月 (电影《摆渡人》插曲)-Beyond.mp3",
+        img: "https://p1.music.126.net/g4HWPlA6pqYixCw6pFZuLg==/109951165460311280.jpg?param=34y34"
+    },
+    {
+        name: "真的爱你",
+        author: "beyond",
+        path: "/music/真的爱你 (电影《摆渡人》插曲)-Beyond.mp3",
+        img: "https://p2.music.126.net/ghmbmEQS-IJfZPjdA3KGxg==/82463372084291.jpg?param=34y34"
+    },
+    {
+        name: "后来的我们",
+        author: "五月天",
+        path: "/music/后来的我们-五月天.mp3",
+        img: "http://p1.music.126.net/_B1Fn_Z1WxHzqGLzLZDf-w==/109951163263882447.jpg?param=34y34"
+    },  {
+        name: "Smile Like You Used To",
+        author: "Tim Walker",
+        path: "/music/Tim Walker - Smile Like You Used To.mp3",
+        img: "http://p3.music.126.net/m9CkDd3lfcR4IFSbBGKq4w==/2542070883708394.jpg?param=34y34"
+        
+    },
+    {
+        name: "海阔天空",
+        author: "beyond",
+        path: "/music/海阔天空 (网络电影《九五2班》插曲)-Beyond.mp3",
+        img: "https://p2.music.126.net/ghmbmEQS-IJfZPjdA3KGxg==/82463372084291.jpg?param=34y34"
+    },
+    {
+        name: "光辉岁月",
+        author: "beyond",
+        path: "/music/光辉岁月 (电影《摆渡人》插曲)-Beyond.mp3",
+        img: "https://p1.music.126.net/g4HWPlA6pqYixCw6pFZuLg==/109951165460311280.jpg?param=34y34"
+    },
+    {
+        name: "真的爱你",
+        author: "beyond",
+        path: "/music/真的爱你 (电影《摆渡人》插曲)-Beyond.mp3",
+        img: "https://p2.music.126.net/ghmbmEQS-IJfZPjdA3KGxg==/82463372084291.jpg?param=34y34"
+    },
+    {
+        name: "后来的我们",
+        author: "五月天",
+        path: "/music/后来的我们-五月天.mp3",
+        img: "http://p1.music.126.net/_B1Fn_Z1WxHzqGLzLZDf-w==/109951163263882447.jpg?param=34y34"
+    },  {
+        name: "Smile Like You Used To",
+        author: "Tim Walker",
+        path: "/music/Tim Walker - Smile Like You Used To.mp3",
+        img: "http://p3.music.126.net/m9CkDd3lfcR4IFSbBGKq4w==/2542070883708394.jpg?param=34y34"
+        
     },
     {
         name: "海阔天空",
@@ -37,32 +98,30 @@ var musics = [
         img: "http://p1.music.126.net/_B1Fn_Z1WxHzqGLzLZDf-w==/109951163263882447.jpg?param=34y34"
     }
 ]
+//默认的设置
 var musicindex = 0;
-//默认的音乐
 $("#palyer").src = musics[musicindex].path;
 $(".m_name")[0].innerText = musics[musicindex].name;
 $(".m-author")[0].innerText = musics[musicindex].author;
+$(".music-num")[0].innerText=musics.length
+$(".music-num-1")[0].innerText=musics.length
 var totaltime = 0;
 // $(".m-totaltime").innerText = "00:00";
 var isplay = false;//给定一个默认的播放状态
 //将时间转化为需要的格式
 function contime(second) {
-    // minu = second > 600 ? parseInt(second / 60.0) : "0" + parseInt(second / 60.0)
-    // sec = parseInt((parseFloat(second / 60.0) - parseInt(second / 60.0)) * 60) < 10 ?
-    //     "0" + parseInt((parseFloat(second / 60.0) - parseInt(second / 60.0)) * 60) :
-    //     parseInt((parseFloat(second / 60.0) - parseInt(second / 60.0)) * 60);
-    // return minu + ":" + sec;
     var date = new Date(second * 1000);//时间戳为10位需*1000，时间戳为13位的话不需乘1000
     minu = ("0" + date.getMinutes()).slice(-2),
-        sec = ("0" + date.getSeconds()).slice(-2);
+    sec = ("0" + date.getSeconds()).slice(-2);
     return minu + ":" + sec;
-
 }
-$(".palyer-paly")[0].onclick = function () {
+$(".palyer-paly")[0].onclick = function () { 
+    totaltime = $("#palyer").duration;
+    $(".m-totaltime")[0].innerText = contime(totaltime)
     if (!isplay) {
         $("#palyer").play();
         setInterval(() => {
-            setBarCur()
+            setBarCur();
         }, 500);
         isplay = true;
     } else {
@@ -88,7 +147,6 @@ function changeplaystatus() {
 function switch_music(m_index) {
     $("#palyer").src = musics[m_index].path;
     $(".music-img")[0].src = musics[m_index].img;
-    //console.log("")
     $("#palyer").oncanplay = function () {
         totaltime = $("#palyer").duration;
         $(".m-totaltime")[0].innerText = contime(totaltime)
@@ -101,13 +159,16 @@ function switch_music(m_index) {
 }
 // 上一首按钮
 $(".palyer-pre")[0].onclick = function () {
+    $("#palyer").currentTime=0;
+    setBarCur()
     musicindex = --musicindex < 0 ? musics.length - 1 : musicindex;
-    switch_music(musicindex)
+    switch_music(musicindex);
 }
 // 下一首按钮
 $(".palyer-next")[0].onclick = function () {
     musicmode();
 }
+
 // 上一首
 function premusic() {
     musicindex = --musicindex < 0 ? musics.length - 1 : musicindex;
@@ -121,7 +182,7 @@ function nextmusic() {
 // 随机下一首
 function randommusic() {
     musicindex = Math.floor(Math.random() * (musics.length));
-    switch_music(musicindex)
+    switch_music(musicindex);
 }
 // 单曲循环
 function singlemusic() {
@@ -137,7 +198,6 @@ var musicmode = randommusic;
 进度条的位置 = 获取当前音乐播放的时长/总长 *首先获取进度条的长度
 */
 function setBarCur() {
-
     var prop = $("#palyer").currentTime / totaltime;
     var maxlength = $(".m-bar")[0].offsetWidth - 2;
     var musiccur = (prop).toFixed(4) * 100 + "%"
@@ -153,10 +213,9 @@ $(".f-btn")[0].onmousedown = function (e) {
     // 拖拽音乐直接播放
     $("#palyer").play();
     isplay = true;
-    changeplaystatus()
+    changeplaystatus();
     // 获取总时间
     totaltime = $("#palyer").duration;
-
     // 鼠标按下并拖拽
     document.onmousemove = function (e) {
         var clickbar;
@@ -174,7 +233,9 @@ $(".f-btn")[0].onmousedown = function (e) {
         // 设置音乐的当前播放时间
         $("#palyer").currentTime = clickbar / $(".m-bar")[0].offsetWidth * totaltime;
         //让进度条跳转
-        setBarCur()
+        setInterval(() => {
+            setBarCur();
+        }, 500);
     }
     document.onmouseup = function () {
         this.onmousedown = null;
@@ -186,10 +247,9 @@ $(".m-bar")[0].onclick = function (e) {
     // 点击音乐直接播放
     $("#palyer").play();
     isplay = true;
-    changeplaystatus()
+    changeplaystatus();
     // 获取总时间
     totaltime = $("#palyer").duration;
-
     var clickbar;
     if (e.screenX - $(".music-bar")[0].offsetLeft < 0) {
         clickbar = 0;
@@ -205,8 +265,10 @@ $(".m-bar")[0].onclick = function (e) {
     // 设置音乐的当前播放时间
     $("#palyer").currentTime = clickbar / $(".m-bar")[0].offsetWidth * totaltime;
     //让进度条跳转
-    setBarCur()
 
+    setInterval(() => {
+        setBarCur();
+    }, 500);
 }
 // 获取点击次数
 var modeclick = 0;
@@ -214,21 +276,21 @@ $(".palyer-mode")[0].onclick = function () {
     modeclick++;
     // 循环播放
     if (modeclick % 3 == 1) {
-        $(".palyer-mode")[0].classList.remove("palyer-mode-default")
-        $(".palyer-mode")[0].classList.add("palyer-mode-cycle")
+        $(".palyer-mode")[0].classList.remove("palyer-mode-default");
+        $(".palyer-mode")[0].classList.add("palyer-mode-cycle");
         musicmode = nextmusic;
     }
     //单曲循环
     else if (modeclick % 3 == 2) {
-        $(".palyer-mode")[0].classList.remove("palyer-mode-cycle")
-        $(".palyer-mode")[0].classList.add("palyer-mode-single")
+        $(".palyer-mode")[0].classList.remove("palyer-mode-cycle");
+        $(".palyer-mode")[0].classList.add("palyer-mode-single");
         musicmode = singlemusic;
 
     }//随机播放
     else {
-        $(".palyer-mode")[0].classList.remove("palyer-mode-single")
-        $(".palyer-mode")[0].classList.add("palyer-mode-default")
-        musicmode = singlemusic;
+        $(".palyer-mode")[0].classList.remove("palyer-mode-single");
+        $(".palyer-mode")[0].classList.add("palyer-mode-default");
+        musicmode = randommusic;
     }
 
 }
@@ -246,4 +308,44 @@ $(".palyer-volume")[0].onclick = function hs(){
     }else{
         $(".volume-bar-bg")[0].style.visibility="hidden";
     }
+}
+// 给音乐列表添加元素
+for(var i = 0;i<musics.length;i++){
+   var lis= document.createElement("li")
+    $(".music-list-name")[0].appendChild(lis)
+    for(var j =0;j<3;j++){
+        var divs = document.createElement("div")
+        document.querySelectorAll(".music-list-name li")[i].appendChild(divs)
+    }
+    document.querySelectorAll(".music-list-name li")[i].getElementsByTagName("div")[0].innerText=musics[i].name;
+    document.querySelectorAll(".music-list-name li")[i].getElementsByTagName("div")[1].innerText=musics[i].author;
+    // document.querySelectorAll(".music-list-name li")[i].getElementsByTagName("div")[2].innerText=musics[i].name;
+}
+// 给列表显示绑定点击事件
+var listclick=1;
+$(".music-num")[0].onclick = function hs(){
+    if(listclick++%2==1){
+        $(".music-list")[0].style.visibility="visible";
+        $(".bg")[0].classList.add("bg-1");
+    }else{
+        $(".music-list")[0].style.visibility="hidden";
+    }
+}
+for(var o = 0;o<musics.length;o++){
+    (function(o){
+    document.querySelectorAll(".music-list-name li")[o].onclick=function (){
+    switch_music(o);
+    }
+    }(o));
+}
+// 给锁定按钮绑定事件
+var lockclick=1;
+$(".lock-btn")[0].onclick =function(){
+    if(lockclick++%2==1){
+        $(".bg")[0].classList.add("bg-1");
+        $(".lock-btn")[0].style.backgroundPosition="-100px -400px";
+    }else{
+        $(".bg")[0].classList.remove("bg-1");
+        $(".lock-btn")[0].style.backgroundPosition="-80px -380px";
+    } 
 }
